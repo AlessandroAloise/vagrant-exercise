e66c61eda51fbbe4bd7bd15d012a4f70df2f969e
Alessandro Aloise <54986411+AlessandroAloise@users.noreply.github.com>
2022-09-28 09:39:30 +0200

    apertura porta

--- scripts/provision_mysql.sh
+++ scripts/provision_mysql.sh
@@ -7,6 +7,12 @@ sudo apt-get install mysql-server -y
 #abilitare le connessioni da altri server
 echo "Updating bind address"
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+# opening database port
+apt-get install ufw -y
+ufw --force enable
+ufw allow 3306
+
 #riavviare mysql in modo da applicare le modifiche
 echo "Restarting mysql service"
 sudo service mysql restart
